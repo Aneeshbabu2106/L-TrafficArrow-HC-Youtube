f0f72022416ff377ad145a757b0989ef98810818
Aneesh Babu <73874288+Aneeshbabu2106@users.noreply.github.com>
2023-07-14 12:21:58 +0530

    updated game p[lay with obstacle mechanics

--- Assets/Scripts/GamePlayManager.cs
+++ Assets/Scripts/GamePlayManager.cs
@@ -1,18 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
 
 public class GamePlayManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region START
+
+    public bool hasGameFinished;
+    public static GamePlayManager Instance;
+
+    private void Awake()
     {
-        
+        Instance = this;
+
+        hasGameFinished = false;
+        GameManager.Instance.Isinitialized = true;
+
+        score = 0;
+        currentLevel = 0;
+        scoreSpeed = _levelSpeed[currentLevel];
+        _scoreTest.text = ((int)score).ToString();
+
+        for (int i=0;i<8;i++)
+        {
+            SpawnObstacle();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    #region SCORE
+
+    private float score;
+    private int currentLevel;
+    private float scoreSpeed;
+
+    [SerializeField] private List<int> _levelSpeed, _levelMax;
+    [SerializeField] private TMP_Text _scoreTest;
+    [SerializeField] private GameObject _obstaclePrefab;
+    [SerializeField] private float _spawnX, _spawnY;
+
+    private void Update()
     {
-        
+        if (hasGameFinished) return;
+        score += scoreSpeed * Time.deltaTime;
+        _scoreTest.text = ((int)score).ToString();
+
+        if (score> _levelMax[Mathf.Clamp(currentLevel,0,_levelMax.Count)])
+        {
+            currentLevel = Mathf.Clamp(currentLevel + 1, 0, _levelMax.Count);
+            SpawnObstacle();
+            scoreSpeed = _levelSpeed[currentLevel];
+        }
+
     }
+    private void SpawnObstacle()
+    {
+        Vector3 spawnPos = new Vector3(UnityEngine.Random.Range(-_spawnX, _spawnX), UnityEngine.Random.Range(-_spawnY, _spawnY), 0f);
+        RaycastHit2D hit = Physics2D.CircleCast(spawnPos, 1f, Vector2.zero);
+        bool canSpawn = hit;
+
+        while(canSpawn)
+        {
+            spawnPos = new Vector3(UnityEngine.Random.Range(-_spawnX, _spawnX), UnityEngine.Random.Range(-_spawnY, _spawnY), 0f);
+            hit = Physics2D.CircleCast(spawnPos, 1f, Vector2.zero);
+            canSpawn = hit;
+        }
+        Instantiate(_obstaclePrefab, spawnPos, Quaternion.Euler(0, 0, UnityEngine.Random.Range(0, 37) * 10f));
+    }
+
+    #endregion
+
+    #region GAME_OVER
+
+    public UnityAction GameEnd;
+
+    [SerializeField] private AudioClip _loseclip;
+
+    public void GameEnded()
+    {
+        SoundManager.Instance.PlaySound(_loseclip);
+        GameEnd?.Invoke();
+        hasGameFinished = true;
+        GameManager.Instance.CurrentScore = (int) score;
+        StartCoroutine(GameOver());
+
+    }
+    private IEnumerator GameOver()
+    {
+        yield return new WaitForSeconds(2f);
+        GameManager.Instance.GotoMainMenu();
+    }
+    #endregion
 }
--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _moveSpeed;
+
+
+    private Vector2 moveDirection;
+
+    private void Awake()
     {
-        
+        moveDirection = (new Vector2(
+            Random.Range(-1f, 1f),
+            Random.Range(-1f, 1f)
+            )).normalized;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        transform.position += (Vector3)(_moveSpeed * Time.fixedDeltaTime * moveDirection);
+
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Side"))
+        {
+            moveDirection.x += -1f;
+        }
+        if (collision.CompareTag("Top"))
+        {
+            moveDirection.y += -1f;
+        }
+    }
+
 }
